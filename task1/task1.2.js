5fae95bb5415e09e7f6999ec414751290412922f
Nick Ilyin <amt.ilyin@gmail.com>
2023-02-28 20:22:27 +0600

    Убирает parseInt и исправляет проверку на NaN

--- task1/task1.2.js
+++ task1/task1.2.js
@@ -1,7 +1,7 @@
 function getNumberRadix(number,radix) {
-  number=parseInt(number);
-  if (number>0 && !isNaN(number) && (typeof radix==='number') && radix>=2 && radix<=16) {
+  number=+number;
+  if (number>0 && Number.isInteger(number) && !Number.isNaN(number) && (typeof radix==='number') && radix>=2 && radix<=16) {
     return number.toString(radix);
   }
-  throw Error("Функция getNumberRadix была вызвана с некорректными параметрами");
+  throw new Error("Функция getNumberRadix была вызвана с некорректными параметрами");
 }
