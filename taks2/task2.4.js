433d91c8f36144a98438cb316b223183a284c597
Nick Ilyin <amt.ilyin@gmail.com>
2023-03-02 17:39:38 +0600

    add checking Number.isNaN()

--- taks2/task2.4.js
+++ taks2/task2.4.js
@@ -1,6 +1,6 @@
 const getUniqArray=(arr)=>{
   for (const el of arr) {
-    if ( typeof el !='number') {
+    if ( typeof el !='number'|| Number.isNaN(el)) {
       throw new Error(`В getUniqArray был передан невалидный параметр. Аргумент arr должен быть массивом чисел`);
     }
   }
