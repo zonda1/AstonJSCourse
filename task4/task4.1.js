10904384211fc730f2735e858252da1ea5e760f6
Nick Ilyin <amt.ilyin@gmail.com>
2023-03-10 15:05:41 +0600

    errors correction task4.1

--- task4/task4.1.js
+++ task4/task4.1.js
@@ -1,13 +1,9 @@
 Array.prototype.filterArray=function (cb,...thisArg) {
-  let newArr=[];
-  if (thisArg.length) {
-    for (const iterator of this) {
-      if (cb.call(...thisArg,iterator)) newArr.push(iterator);
-      continue;
-    }
-  }
-  for (const iterator of this) {
-    if (cb(iterator)) newArr.push(iterator);
+  const arr = this;
+  const newArr = [];
+  const len = arr.length;
+  for (let i = 0; i < len; i++) {
+    if (cb.call(...thisArg, arr[i], i, arr)) newArr.push(arr[i]);
     continue;
   }
   return newArr;
